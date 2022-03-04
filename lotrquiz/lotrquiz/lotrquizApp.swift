272a188d41938768443f63b2be0fc2759fc5b1c8
José Ibáñez <joseibaben35@gmail.com>
2022-03-04 18:07:15 +0100

    Project creation

---
+++ lotrquiz/lotrquiz/lotrquizApp.swift
@@ -0,0 +1,17 @@
+//
+//  lotrquizApp.swift
+//  lotrquiz
+//
+//  Created by José Ibáñez Bengoechea on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct lotrquizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
