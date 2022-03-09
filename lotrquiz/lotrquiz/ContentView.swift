ac62ec85401778a0bf6be03253c4a0e39f1fa2e4
José Ibáñez <jose@develaw.com>
2022-03-09 23:35:54 +0100

    Ring masked

--- lotrquiz/lotrquiz/ContentView.swift
+++ lotrquiz/lotrquiz/ContentView.swift
@@ -7,6 +7,36 @@
 
 import SwiftUI
 
+struct Arc : InsettableShape {
+    var startAngle: Angle
+    var tickingAmount : Double
+    var clockwise : Bool
+    var insetAmount = 0.0
+    
+    var animatableData: Double {
+        get { tickingAmount }
+        set { tickingAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        
+        var path = Path()
+        let rotationAdjustment = Angle(degrees: 90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = .degrees(tickingAmount) - rotationAdjustment
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.midX, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+   
+}
+
 struct ContentView: View {
     var allQuestions : [Question] = Bundle.main.decode("questions.json")
     @State private var gameQuestions = ArraySlice<Question>()
@@ -16,66 +46,127 @@ struct ContentView: View {
     @State private var selectedOption = ""
     @State private var options = [String]()
         
-   
+    @State private var tickingAmount = 0.0
+    @State private var remaining = 30
     
     
     var body: some View {
         NavigationView{
-            if (gameQuestions != []){
-                VStack{
-                    VStack(alignment: .center){
-                        Text(gameQuestions[currentRound].question)
-                            .font(.headline)
-                    }
-                    .padding()
-                    ForEach(options, id: \.self){option in
-                        Button{
-                            answerQuestion(option)
-                            
-                        } label: {
-                            Text(option)
-                                .frame(maxWidth: .infinity, maxHeight: 40)
-                                .background(resolveColor(option))
-                                .foregroundColor(.white)
-                                .padding(.horizontal)
+            ZStack{
+                Image("background")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                Color(.black)
+                    .opacity(0.75)
+                    .ignoresSafeArea()
+                if (gameQuestions != []){
+                    VStack{
+                        ZStack{
+                            Text(remaining, format: .number)
+                                .font(.title)
+                                .frame(maxWidth: 165, maxHeight: 165)
+                            if(tickingAmount > 10.0){
+                                Image("ringrule")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(maxWidth: 165, maxHeight: 165)
+                                    .colorMultiply(ringColor())
+                                    .mask{
+                                        Arc(startAngle: .degrees(0), tickingAmount: tickingAmount, clockwise: true)
+                                            .strokeBorder(.red, style: StrokeStyle(lineWidth: 35, lineCap: .round, lineJoin: .round))
+                                            .frame(maxWidth: 165, maxHeight: 165)
+                                    }
+                            }
+                        }
+                        
+                        VStack(alignment: .center){
+                            Text(gameQuestions[currentRound].question)
+                                .font(.custom("anirb___", size: 36))
+                        }
+                        .padding()
+                        ForEach(options, id: \.self){option in
+                            Button{
+                                Task.init(priority: .high) {
+                                    await answerQuestion(option)
+                                }
+                            } label: {
+                                Text(option)
+                                    .frame(maxWidth: .infinity, maxHeight: 50)
+                                    .foregroundColor(.white)
+                                    .font(.title3)
+                                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .strokeBorder(resolveColor(option), lineWidth: 2)
+                                    )
+                                    .padding(.horizontal)
+                            }
                         }
                     }
+                    .navigationTitle("El señor de los anillos")
+                    .font(.custom("anirb___.ttf", size: 36))
+                    .frame(maxWidth: 550)
+                    .preferredColorScheme(.dark)
                 }
-                .navigationTitle("El señor de los anillos")
-                .frame(maxWidth: 550)
             }
+            
            
         }
         .onAppear{
             gameQuestions = allQuestions.shuffled()[..<10]
             options = gameQuestions[currentRound].options.shuffled()
+            
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                withAnimation(.linear(duration: 1)){
+                    if (remaining > 0 && !answered) {
+                        tickingAmount += 12
+                        remaining -= 1
+                    } else {
+                        Task.init(priority: .high) {
+                            await answerQuestion("")
+                        }
+                    }
+                    
+                }
+            }
         }
     }
     
-    func answerQuestion(_ option: String) {
+    func answerQuestion(_ option: String) async {
         withAnimation{
             selectedOption = option
             answered = true
         }
-        Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { _ in
-            currentRound += 1
-            selectedOption = ""
-            answered = false
-            options = gameQuestions[currentRound].options.shuffled()
-        }
+        try? await Task.sleep(nanoseconds: 2_000_000_000)
+        remaining = 30
+        currentRound += 1
+        selectedOption = ""
+        answered = false
+        options = gameQuestions[currentRound].options
+        tickingAmount = 0.0
         
+    }
+    
+    func ringColor() -> Color {
+        if(remaining > 20) {
+            return .white
+        } else if (remaining > 10) {
+            return .orange
+        }
+        return .red
     }
     
     func resolveColor(_ option: String) -> Color {
         if(!answered){
-            return .blue
+            return Color(red: 168/255, green: 147/255, blue: 36/255, opacity: 0.6)
         }
         if(option == gameQuestions[currentRound].correctAnswer){
             return .green
         } else if (option != gameQuestions[currentRound].correctAnswer && option == selectedOption){
             return .red
         }
-        return .blue
+        return Color(red: 168/255, green: 147/255, blue: 36/255, opacity: 0.6)
     }
         
 }
